bd3ca5ce9d9e1397c444a0dc9edafa7f520d3e25
unknown <luana.2022318861@aluno.iffar.edu.br>
2024-06-18 11:29:18 -0300

    aula 16/06

--- app/Views/master.php
+++ app/Views/master.php
@@ -1,9 +1,36 @@
-	<?= $this->include('partials/header') ?>
-	 		
-    <?= $this->renderSection('content') ?>
-	
-    <?= $this->include('partials/footer') ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Modern Business - Start Bootstrap Template</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="<?= base_url('public/assets/assets/favicon.ico') ?>" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="<?= base_url('public/css/styles.css') ?>" rel="stylesheet" />
+</head>
+
+<body class="d-flex flex-column h-100">
+    <main class="flex-shrink-0">
+
+        <?= $this->include('partials/navbar') ?>
+
+        
+        <div>
+            <?= $this->renderSection('content') ?>
+        </div>
 
 
+        <?= $this->include('partials/footer') ?>
 
 
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="<?= base_url('public/assets/js/scripts.js') ?>"></script>
+</body>
+
+</html>
--- app/Views/pagina_inicial.php
+++ app/Views/pagina_inicial.php
@@ -1,349 +1,149 @@
 <?= $this->extend('master')?>
 <?= $this->section('content')?>
-
-<link rel="stylesheet" href="css/bootstrap.min.css">
-    <title>Login</title>
-    <style>
-      <!DOCTYPE html>
-<html lang="pt-br">
-
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Controle e Contagem de Estoque</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <!-- Adicionando ícones FontAwesome -->
-    <style>
-        /* Estilos gerais */
-        body {
-            margin: 0;
-            font-family: Arial, sans-serif;
-            background-color: #f2f2f2;
-            position: relative;
-        }
-
-
-        .container {
-            display: flex;
-            flex-wrap: wrap;
-            padding: 20px;
-        }
-
-
-        /* Estilos da tabela */
-        .table-container {
-            margin-top: 90px;
-            width: 100%;
-            overflow-x: auto;
-            /* Adicionando rolagem horizontal para a tabela em telas pequenas */
-        }
-
-
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-bottom: 20px;
-        }
-
-
-        th,
-        td {
-            border: 1px solid #ddd;
-            padding: 10px;
-            text-align: left;
-        }
-
-
-        th {
-            background-color: #f2f2f2;
-        }
-
-
-        tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
-
-
-        tr:hover {
-            background-color: #ddd;
-        }
-
-
-
-
-
-
-
-
-        /* Estilos da barra lateral */
-        .sidebar {
-            width: 100%;
-            max-width: 300px;
-            /* Limitando a largura máxima da barra lateral */
-            background-color: #fff;
-            padding: 20px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            /* Adicionando sombra */
-            margin-bottom: 20px;
-        }
-
-
-
-
-
-
-
-
-        .sidebar h1 {
-            font-size: 24px;
-            margin-bottom: 20px;
-        }
-
-
-
-
-
-
-
-
-        .sidebar button {
-            display: flex;
-            align-items: center;
-            margin-bottom: 10px;
-            padding: 10px;
-            width: 100%;
-            text-align: left;
-            background-color: #f2f2f2;
-            border: none;
-            cursor: pointer;
-            border-radius: 5px;
-        }
-
-
-        .sidebar button:hover {
-            background-color: #e0e0e0;
-        }
-
-
-
-
-
-
-
-
-        /* Estilos do ícone */
-        .sidebar .icon {
-            margin-right: 10px;
-        }
-
-
-
-
-
-
-
-
-        /* Estilos da imagem */
-        .logo {
-            width: 100%;
-            max-width: 250px;
-            height: auto;
-            margin-bottom: 20px;
-        }
-
-
-
-
-        /*Estilo da barra de busca*/
-        #divBusca {
-            background-color: #E0EEEE;
-            border: solid 2px #1a8ae5;
-            background-color: #bbdffb;
-            border-radius: 10px;
-
-
-            width: 400px;
-            height: 32px;
-            position: absolute;
-            /* Posicionamento absoluto */
-            bottom: 350px;
-           margin-left: 600px;
-            transform: translateX(-50%);
-            /* Ajustando para centralizar */
-            margin-top: 70px;
-            /* Ajustando para sobrepor à tabela */
-        }
-
-
-
-
-        #txtBusca {
-            float: left;
-            background-color: transparent;
-            padding-left: 118px;
-            font-size: 18px;
-            border: none;
-            height: 30px;
-            width: 110px;
-        }
-
-
-
-
-        #btnBusca {
-            border: none;
-            float: right;
-            height: 32px;
-            border-radius: 0 7px 7px 0;
-            width: 70px;
-            font-weight: bold;
-            background: #1a8ae5;
-        }
-
-
-
-
-        #divBusca img {
-            float: left;
-        }
-
-
-
-
-        /* Estilo do botão de relatório */
-        #btnRelatorio {
-            position: absolute;
-            top: 20px;
-            right: 20px;
-            background-color: #1a8ae5;
-            color: #fff;
-            border: none;
-            padding: 10px 20px;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-
-
-
-        @media only screen and (min-width: 768px) {
-            .container {
-                justify-content: space-between;
-                /* Posiciona os elementos lado a lado */
-                padding: 20px 40px;
-            }
-
-
-            .table-container {
-                flex: 1;
-                /* A tabela ocupa todo o espaço restante */
-                margin-left: 20px;
-                /* Adiciona espaço entre a barra lateral e a tabela */
-            }
-        }
-    </style>
-</head>
-
-
-<body>
-
-
-
-
-
-
-
-
-    <div class="container">
-        <div class="sidebar">
-            <img class="logo" src="logoppi.png" alt="Logo PPI">
-            <h1>Menu</h1>
-            <button><i class="fas fa-home icon"></i> Início</button>
-            <button><i class="fas fa-pencil-alt icon"></i> Ajustes de Inventário</button>
-            <button><i class="fas fa-archive icon"></i> Estoques</button>
-            <button><i class="fas fa-coins icon"></i> Compra e Venda</button>
-            <button><i class="fas fa-users icon"></i> Fornecedores</button>
-        </div>
-
-
-
-
-
-
-
-
-        <div id="divBusca">
-            <alt="Buscar..." />
-            <input type="text" id="txtBusca" placeholder="Buscar..." />
-            <button id="btnBusca">Buscar
-            </button>
-        </div>
-
-
-
-
-        <button id="btnRelatorio">Relatório</button>
-
-
-
-
-        <div class="table-container">
-
-
-            <table>
-                <thead>
-                    <tr>
-                        <th>Código</th>
-                        <th>Nome</th>
-                        <th>Marca</th>
-                        <th>Modelo</th>
-                        <th>Cor</th>
-                        <th>Preço Unitário</th>
-                        <th>Quantidade Estoque</th>
-                        <th>Local de Armazenamento</th>
-                        <th>Comentário</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <!-- Linhas com células vazias para nova tabela -->
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <!-- Repita as linhas acima para adicionar mais linhas conforme necessário -->
-                </tbody>
-            </table>
-
-
-            <!-- Adicione mais tabelas conforme necessário -->
-
-
-        </div>
-</body>
-
-
-</html>
-
-
-
-
-<?= $this->endSection()?>
+<section class="py-5" id="features">
+                <div class="container px-5 my-5">
+                    <div class="row gx-5">
+                        <div class="col-lg-4 mb-5 mb-lg-0"><h2 class="fw-bolder mb-0">A better way to start building.</h2></div>
+                        <div class="col-lg-8">
+                            <div class="row gx-5 row-cols-1 row-cols-md-2">
+                                <div class="col mb-5 h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>
+                                    <h2 class="h5">Featured title</h2>
+                                    <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                                </div>
+                                <div class="col mb-5 h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
+                                    <h2 class="h5">Featured title</h2>
+                                    <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                                </div>
+                                <div class="col mb-5 mb-md-0 h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
+                                    <h2 class="h5">Featured title</h2>
+                                    <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                                </div>
+                                <div class="col h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
+                                    <h2 class="h5">Featured title</h2>
+                                    <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Testimonial section-->
+            <div class="py-5 bg-light">
+                <div class="container px-5 my-5">
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-lg-10 col-xl-7">
+                            <div class="text-center">
+                                <div class="fs-4 mb-4 fst-italic">"Working with Start Bootstrap templates has saved me tons of development time when building new projects! Starting with a Bootstrap template just makes things easier!"</div>
+                                <div class="d-flex align-items-center justify-content-center">
+                                    <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
+                                    <div class="fw-bold">
+                                        Tom Ato
+                                        <span class="fw-bold text-primary mx-1">/</span>
+                                        CEO, Pomodoro
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Blog preview section-->
+            <section class="py-5">
+                <div class="container px-5 my-5">
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-lg-8 col-xl-6">
+                            <div class="text-center">
+                                <h2 class="fw-bolder">From our blog</h2>
+                                <p class="lead fw-normal text-muted mb-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row gx-5">
+                        <div class="col-lg-4 mb-5">
+                            <div class="card h-100 shadow border-0">
+                                <img class="card-img-top" src="https://dummyimage.com/600x350/ced4da/6c757d" alt="..." />
+                                <div class="card-body p-4">
+                                    <div class="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
+                                    <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-3">Blog post title</h5></a>
+                                    <p class="card-text mb-0">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                                    <div class="d-flex align-items-end justify-content-between">
+                                        <div class="d-flex align-items-center">
+                                            <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
+                                            <div class="small">
+                                                <div class="fw-bold">Kelly Rowan</div>
+                                                <div class="text-muted">March 12, 2023 &middot; 6 min read</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-5">
+                            <div class="card h-100 shadow border-0">
+                                <img class="card-img-top" src="https://dummyimage.com/600x350/adb5bd/495057" alt="..." />
+                                <div class="card-body p-4">
+                                    <div class="badge bg-primary bg-gradient rounded-pill mb-2">Media</div>
+                                    <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-3">Another blog post title</h5></a>
+                                    <p class="card-text mb-0">This text is a bit longer to illustrate the adaptive height of each card. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                                    <div class="d-flex align-items-end justify-content-between">
+                                        <div class="d-flex align-items-center">
+                                            <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
+                                            <div class="small">
+                                                <div class="fw-bold">Josiah Barclay</div>
+                                                <div class="text-muted">March 23, 2023 &middot; 4 min read</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-5">
+                            <div class="card h-100 shadow border-0">
+                                <img class="card-img-top" src="https://dummyimage.com/600x350/6c757d/343a40" alt="..." />
+                                <div class="card-body p-4">
+                                    <div class="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
+                                    <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-3">The last blog post title is a little bit longer than the others</h5></a>
+                                    <p class="card-text mb-0">Some more quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                                    <div class="d-flex align-items-end justify-content-between">
+                                        <div class="d-flex align-items-center">
+                                            <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
+                                            <div class="small">
+                                                <div class="fw-bold">Evelyn Martinez</div>
+                                                <div class="text-muted">April 2, 2023 &middot; 10 min read</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Call to action-->
+                    <aside class="bg-primary bg-gradient rounded-3 p-4 p-sm-5 mt-5">
+                        <div class="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
+                            <div class="mb-4 mb-xl-0">
+                                <div class="fs-3 fw-bold text-white">New products, delivered to you.</div>
+                                <div class="text-white-50">Sign up for our newsletter for the latest updates.</div>
+                            </div>
+                            <div class="ms-xl-4">
+                                <div class="input-group mb-2">
+                                    <input class="form-control" type="text" placeholder="Email address..." aria-label="Email address..." aria-describedby="button-newsletter" />
+                                    <button class="btn btn-outline-light" id="button-newsletter" type="button">Sign up</button>
+                                </div>
+                                <div class="small text-white-50">We care about privacy, and will never share your data.</div>
+                            </div>
+                        </div>
+                    </aside>
+                </div>
+            </section>
+        </main>
+<?= $this->endSection('content')?>
